972e9aa49e7aaaf2086f9605e49302a12eac0cc9
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-10-04 10:11:46 -0400

    add file

--- hw3-1-2.py
+++ hw3-1-2.py
@@ -1,7 +1,9 @@
+#author CJP 9/29/2021
 firstcardvalue = int(input("What the value of your first card? "))
 secondcardvalue= int(input("What is the value of the second card? "))
 
 if (firstcardvalue + secondcardvalue) <= 21:
     print("You are safe!")
-
+else:
+    print("Bust!")
 
